8ae1c3c0770f33205f84a6827178f21374024c43
JHE H <tetsu@DESKTOP-B3TM6VV.localdomain>
2022-09-04 16:08:54 +0800

    #2 solved

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import {
 } from "react-router-dom";
 import { Home } from './components/Home';
 import { About } from './components/About';
+import { NavMenu } from './components/NavMenu';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavMenu/>
       <div className='content'>
       <Routes>
       <Route path="/" element={<Home />} />
