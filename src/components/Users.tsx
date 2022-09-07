db3b5d979d87a824be52fa329e080edfd9109b69
JHE H <tetsu@DESKTOP-B3TM6VV.localdomain>
2022-09-07 21:35:50 +0800

    finish pagination feature

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -5,17 +5,16 @@ import { UserList } from '../data/UserList';
 export function Users() {
     const [pageIndex, setCurrentPage] = useState(1)
 
-    let userData = UserList.slice(11, 21).map((item, index) => {
+    let userData = UserList.slice((pageIndex - 1) * 10, pageIndex * 10).map((item, index) => {
         // To get range of array items, Use array.slice(start: int, end: int)        
         return <tr key={index}><th scope='row'>{index + 1}</th><td>{item.title}</td></tr>
     })
 
     // downlevelIteration: [...Array(10).keys()].map((index)=> ++index )
-    let span: number = Math.floor(UserList.length / 10)
-    let pageList = Array.from(Array(span).keys()).map((i)=> 
-    {
+    let span: number = Math.floor(UserList.length / 10) + 1
+    let pageList = Array.from(Array(span).keys()).map((i) => {
         ++i
-        return <PaginationItem><PaginationLink href="#">{i}</PaginationLink></PaginationItem>
+        return <PaginationItem key={i}><PaginationLink onClick={() => setCurrentPage(pre => pre = i)}>{i}</PaginationLink></PaginationItem>
     })
 
     console.log(pageList)
@@ -23,7 +22,7 @@ export function Users() {
     return (
         <>
             records length: {UserList.length}
-            <br/>
+            <br />
             Current Page: {pageIndex}
             <Table>
                 <thead>
@@ -41,7 +40,16 @@ export function Users() {
                 </tbody>
             </Table>
             <Pagination>
+                <PaginationItem>
+                    <PaginationLink onClick={() => setCurrentPage(pre => pre = 1)}>
+                        &#60;&#60;
+                    </PaginationLink>
+                </PaginationItem>
                 {pageList}
+                <PaginationItem>
+                    <PaginationLink onClick={() => setCurrentPage(pre => pre = span)}>
+                        &#62;&#62;
+                    </PaginationLink></PaginationItem>
             </Pagination>
         </>
     );
